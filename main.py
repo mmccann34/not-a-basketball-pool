8686038ec146a8dcc3f3f993ea04589945db906a
bballan <bballan>
2013-03-06 01:02:30 -0500

    User Similarity In the middle
    Trying to figure out the jinja issues still, but barebones is there.

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ import string
 import StringIO
 import math
 import json
+import itertools
 from datetime import datetime
 from datetime import timedelta
 from operator import attrgetter
@@ -1565,6 +1566,37 @@ class UpdateYear(BaseHandler):
       p.year = 2013
       p.put()
 
+class UserSimilarity(BaseHandler):
+	def get(self, pool_id):
+		if not self.user:
+			self.require_login()
+			return
+		
+		pool_id = int(pool_id)
+		pool = Pool.by_id(pool_id)
+		gameMatches = 0  #initialize variable to track matches with other users
+
+		if not pool:
+			self.error(404)
+		else:
+			if self.user.id in pool.users:
+				sameGamePicks = {}
+				for e1 in Entry.by_pool(pool_id):
+					sameGamePicks[e1.id] = {}
+					for e2 in Entry.by_pool(pool_id):
+						for game in range(63):
+							if (e1.picks[game] == e2.picks[game]):
+								gameMatches += 1
+						sameGamePicks[e1.id][e2.id] = gameMatches
+						gameMatches = 0
+				self.write(str(sameGamePicks)+ '<br>')
+		
+		params = dict()
+		params['pool'] = pool
+		params['gamePicks'] = sameGamePicks
+		self.render('usersimilarity.html', **params)
+
+
 config = {}
 config['webapp2_extras.sessions'] = {
     'secret_key': 'mavlkjhasehffcsasldfkj',
@@ -1579,6 +1611,7 @@ app = webapp2.WSGIApplication([('/', Front),
                                ('/pools/new', NewPool),
                                ('/pools/all', AllPools),
                                ('/pools/([0-9]+)', PoolPage),
+                               ('/pools/([0-9]+)/usersimilarity', UserSimilarity), #User Similarity Page per Pool
                                ('/pools/([0-9]+)/admin', PoolAdmin),
                                ('/pools/([0-9]+)/admin/export-picks', PoolExportPicks),
                                ('/pools/([0-9]+)/master', PoolMasterBracket),
